e3991f8c87b946f3ec31d1b8f697a37d195e7b5a
Olle Kaiser <olle.kaiser@learnet.se>
2019-09-20 09:26:28 +0200

    log test

--- SlackAPI.js
+++ SlackAPI.js
@@ -12,6 +12,7 @@ class SlackAPI{
                 } else {
                     console.log("SENT")
                     res.json(SlackJSON.SlackResponse("Please register an account and link it before using slash commands", [SlackJSON.SlackAttachement("https://hs.ygstr.com")]))
+                    console.log(SlackJSON.SlackResponse("Please register an account and link it before using slash commands", [SlackJSON.SlackAttachement("https://hs.ygstr.com")]))
                 }
             }
         })

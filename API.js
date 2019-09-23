851ccce289294542cd15cdc27a28f90b1a8823a2
Olle Kaiser <olle.kaiser@learnet.se>
2019-09-23 09:49:51 +0200

    Checked in internal function redo

--- API.js
+++ API.js
@@ -8,10 +8,6 @@ class API{
         res.end("This API call is not implemented yet.")
     }
 
-    async checkout(req, res){
-        res.end("This API call is not implemented yet.")
-    }
-
     async add(req, res){
         res.end("This API call is not implemented yet.")
     }
@@ -24,12 +20,9 @@ class API{
         res.end("This API call is not implemented yet.")
     }
 
-    async check(req, res){
-        res.end("This API call is not implemented yet.")
-    }
-
     async login(req, res){
-        res.end("This API call is not implemented yet.")
+        var username = req.body.username
+        var password = req.body.password
     }
 }
 
